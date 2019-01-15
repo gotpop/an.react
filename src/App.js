497ddc1d41d375afb3394227efde7faa2df67654
gotpop <liam@gotpop.net>
2019-01-15 12:36:14 +0000

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
 import './styles/css/App.css';
 import Header from './components/Header';
-import Main from './components/Main';
 import Footer from './components/Footer';
+import Home from './components/Home';
+import Step1 from './components/Step1';
+import Step2 from './components/Step2';
+import Step3 from './components/Step3';
+
 
 class App extends Component {
   
@@ -30,11 +35,16 @@ class App extends Component {
 
   render() {
     return (
-      <div className="ap">
-            <Header menuItems={this.state.menuItems} />
-            <Main  />
-            <Footer />
-      </div>
+      <BrowserRouter>
+        <div className="ap">
+          <Header menuItems={this.state.menuItems} />
+          <Route exact path="/" component={Home} />
+          <Route path="/step1" render={ () => <Step1 title="This is step 1 !!!" />} />
+          <Route path="/step2" component={Step2} />
+          <Route path="/step3" component={Step3} />
+          <Footer />
+        </div>
+      </BrowserRouter>
     );
   }
 }
