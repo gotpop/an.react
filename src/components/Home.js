910fe9a5d474661c8627cf21cbc97169953d27c3
Liam pop <liam@gotpop.net>
2019-01-17 17:32:57 +0000

    Updated styling and icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,8 +11,11 @@ class Home extends Component {
     render() {
       return (
         <main className="ap__route ma">
-          <h2 className="ma_title">{ this.state.text }</h2>
-          <p>This is a multi step form demo built with React and CSS Grid.</p>
+          <section className="ma__content">
+            <h2 className="ma_title">{ this.state.text }</h2>
+            <p>This is a multi step form demo built with React and CSS Grid.</p>
+            <p>You can add a user by clicking on the link below. Fill out each step and you will see the summary displayed on the final page. If you prefer you can use the links in the header to navigate thorugh the application at your convenience.</p>
+          </section>
           <Link className="btn btn--primary" to="/step1">Get started!</Link>
         </main>
       );
