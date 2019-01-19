90892a2377e2c00c52d8cae2562d5138c459bb41
gotpop <liam@gotpop.net>
2019-01-19 15:34:34 +0000

    Summary table vars work

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -21,39 +21,39 @@ class Summary extends Component {
             <p>Here's the user.</p>
             <ul className="summary__list">
               <li className="summary__item">
-                <span className="summary__key">First name</span>
+                <span className="summary__key">First name:</span>
                 <span className="summary__value">{ this.props.formDetails.firstName }</span>
               </li>
               <li className="summary__item">
-                <span className="summary__key">Last name</span>
+                <span className="summary__key">Last name:</span>
                 <span className="summary__value">{ this.props.formDetails.lastName }</span>
               </li>
               <li className="summary__item">
-                <span className="summary__key">Email</span>
+                <span className="summary__key">Email:</span>
                 <span className="summary__value">{ this.props.formDetails.email }</span>
               </li>
               <li className="summary__item">
-                <span className="summary__key">Address 1</span>
+                <span className="summary__key">Address 1:</span>
                 <span className="summary__value">{ this.props.formDetails2.address1 }</span>
               </li>
               <li className="summary__item">
-                <span className="summary__key">Address 2</span>
+                <span className="summary__key">Address 2:</span>
                 <span className="summary__value">{ this.props.formDetails2.address2 }</span>
               </li>
               <li className="summary__item">
-                <span className="summary__key">Post code</span>
+                <span className="summary__key">Post code:</span>
                 <span className="summary__value">{ this.props.formDetails2.postcode }</span>
               </li>
               <li className="summary__item">
-                <span className="summary__key">User name</span>
+                <span className="summary__key">User name:</span>
                 <span className="summary__value">{ this.props.formDetails3.username }</span>
               </li>
               <li className="summary__item">
-                <span className="summary__key">Password</span>
+                <span className="summary__key">Password:</span>
                 <span className="summary__value">{ this.props.formDetails3.password }</span>
               </li>
               <li className="summary__item">
-                <span className="summary__key">Confirm Password</span>
+                <span className="summary__key">Confirm Password:</span>
                 <span className="summary__value">{ this.props.formDetails3.confirmPassword }</span>
               </li>
             </ul>
